f5aff3a668f02a3da8a216a0d2ed287f3555963f
Per-Emil Johansson <chimikoo@gmail.com>
2025-06-12 04:14:59 +0200

    vercel changes

--- app/api/cart/route.js
+++ app/api/cart/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import connectDB from '@/lib/mongodb';
+import connectDB from '@/lib/dbConnect';
 import Cart from '@/models/Cart';
 
 // GET /api/cart?userId=123
